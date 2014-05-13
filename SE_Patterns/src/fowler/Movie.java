f317e475fb354d115a6c82dfd44237256c707725
Itzepredator <Heinen-andreas@web.de>
2014-05-13 12:50:13 +0200

    Ende der Übung

--- SE_Patterns/src/fowler/Movie.java
+++ SE_Patterns/src/fowler/Movie.java
@@ -4,9 +4,7 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
-    
-    
+    private int priceCode;    
     int getFrequentRenterPoints(int daysRented) {
     	return _price.getFrequentRenterPoints(daysRented);
     	}
