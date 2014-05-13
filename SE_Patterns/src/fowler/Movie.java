ed562d9f5c9dab55458cdcaa10b168a3ef46c780
Itzepredator <Heinen-andreas@web.de>
2014-05-13 12:33:59 +0200

    sdfghjk

--- SE_Patterns/src/fowler/Movie.java
+++ SE_Patterns/src/fowler/Movie.java
@@ -5,6 +5,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String title;
     private int priceCode;
+    
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         priceCode = newpriceCode;
